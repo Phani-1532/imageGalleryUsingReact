53613be1b499eb5eb90e154ab200e9dc8eadd05d
Phanindra <phanindragadireddy2@gmail.com>
2025-01-03 10:58:24 +0530

    updated image gallery

--- src/Gallery.js
+++ src/Gallery.js
@@ -7,7 +7,7 @@ const Gallery = ({photos}) => {
             <div className='row'>
                 {photos.map((image) => 
                 <div key={image.id}>
-                    <div className='col-md-4 img'>
+                    <div className='col img'>
                     <img src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}_m.jpg`} alt={image.description} height='200' width='250' />
                     </div>
                 </div>)}
