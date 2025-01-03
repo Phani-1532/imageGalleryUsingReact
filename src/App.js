28244f7b82c7bd70a539383f2f9b897d18188c95
Phanindra <phanindragadireddy2@gmail.com>
2025-01-03 10:54:47 +0530

    image gallery

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import Gallery from './Gallery';
+const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
+
+
+function App() {
+  const [search, setSearch] = useState()
+  const [data, setData] = useState([])
+
+  const changeHandler = e => {
+    setSearch(e.target.value)
+  }
+
+  const submitHandler = e =>{
+    e.preventDefault()
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${search}&per_page=24&format=json&nojsoncallback=1`).then(
+      response => setData(response.data.photos.photo)
+    )
+  }
+
+  return (
+    <>
+      <div>
+        <center>
+          <h1>Image Gallery Using React!</h1>
+          <form onSubmit={submitHandler}>
+          <input type='text' placeholder='Search Here....' value={search} onChange={changeHandler} />
+          <input type='submit' name='search' />
+          </form>
+          <br></br>
+        </center>
+        {data.length >= 1 ? <Gallery photos={data} /> : <h2>Your desired data is Loading.....</h2>} 
+
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Gallery.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Gallery.css'
+
+const Gallery = ({photos}) => {
+  return (
+         <div>
+            <div className='row'>
+                {photos.map((image) => 
+                <div key={image.id}>
+                    <div className='col-md-4 img'>
+                    <img src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}_m.jpg`} alt={image.description} height='200' width='250' />
+                    </div>
+                </div>)}
+            </div>
+        </div>
+  )
+}
+
+export default Gallery
